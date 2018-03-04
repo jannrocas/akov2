c0ade9b478f51f37fd2a877f04ac67d36bfc487f
Jann Peter Rocas <jannrocas@gmail.com>
2018-03-04 20:43:41 +0800

    first commit

---
+++ client/src/components/Info.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="info">
+    <h3>JannRocas</h3>
+
+    <div v-for="item in info">
+      <p> {{item.title}} : {{item.description}} </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import InfoService from '@/services/InfoService'
+
+export default {
+  name: 'info',
+  data () {
+    return {
+      info: []
+    }
+  },
+  mounted () {
+    this.getInfo()
+  },
+  methods: {
+    async getInfo () {
+      const response = await InfoService.fetchInfo()
+      this.info = response.data
+    }
+  }
+}
+
+</script>
